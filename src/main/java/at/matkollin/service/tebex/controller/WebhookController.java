d663a31b28ca7ad81f385bbd86e97d71573d7163
Matthias Kopeinigg <matkollin@gmail.com>
2022-11-06 00:10:05 +0100

    Add webhook endpoint

--- src/main/java/at/matkollin/service/tebex/controller/WebhookController.java
+++ src/main/java/at/matkollin/service/tebex/controller/WebhookController.java
@@ -3,12 +3,14 @@ package at.matkollin.service.tebex.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/tebex")
 @RestController
 public class WebhookController {
 
-  @PostMapping
+  @PostMapping("/webhook")
   public ResponseEntity<HttpStatus> webhook() {
     return ResponseEntity.ok().build();
   }
