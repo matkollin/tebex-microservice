6aba343a8e93a7e3252a9563947f466d76ef5596
Matthias Kopeinigg <matkollin@gmail.com>
2022-11-06 00:14:24 +0100

    Add constants

--- src/main/java/at/matkollin/service/tebex/TebexServerApplication.java
+++ src/main/java/at/matkollin/service/tebex/TebexServerApplication.java
@@ -1,10 +1,15 @@
 package at.matkollin.service.tebex;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TebexServerApplication {
+
+  public static Gson GSON = new GsonBuilder().create();
+  public static String TEBEX_URL = "https://plugin.tebex.io";
 
   public static void main(String[] args){
     SpringApplication.run(TebexServerApplication.class, args);
