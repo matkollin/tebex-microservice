5bdbb29d8418076c61a1e0de08bebf29a92189c9
Matthias Kopeinigg <matkollin@gmail.com>
2022-11-05 22:53:33 +0100

    Add boot class

---
+++ src/main/java/at/matkollin/service/tebex/TebexServerApplication.java
@@ -0,0 +1,13 @@
+package at.matkollin.service.tebex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TebexServerApplication {
+
+  public static void main(String[] args){
+    SpringApplication.run(TebexServerApplication.class, args);
+  }
+
+}
