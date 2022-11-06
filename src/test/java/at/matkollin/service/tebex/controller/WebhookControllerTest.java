c8a5ac10e13899dae499d93cf0380e951188cf09
Matthias Kopeinigg <matkollin@gmail.com>
2022-11-06 17:56:55 +0100

    Fix mock injection

--- src/test/java/at/matkollin/service/tebex/controller/WebhookControllerTest.java
+++ src/test/java/at/matkollin/service/tebex/controller/WebhookControllerTest.java
@@ -4,15 +4,15 @@ import at.matkollin.service.tebex.dto.webhook.PaymentDTO;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = WebhookController.class)
 public class WebhookControllerTest {
 
-  @MockBean
+  @InjectMocks
   private WebhookController webhookController;
 
   @Test
