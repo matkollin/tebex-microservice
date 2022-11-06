f0ee4a590a4111c3fc1848dff1fe849016489516
Matthias Kopeinigg <matkollin@gmail.com>
2022-11-06 21:41:08 +0100

    Remove unused field

--- src/main/java/at/matkollin/service/tebex/TebexServerApplication.java
+++ src/main/java/at/matkollin/service/tebex/TebexServerApplication.java
@@ -9,9 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TebexServerApplication {
 
   public static Gson GSON = new GsonBuilder().create();
-  public static String TEBEX_URL = "https://plugin.tebex.io";
 
-  public static void main(String[] args){
+  public static void main(String[] args) {
     SpringApplication.run(TebexServerApplication.class, args);
   }
 
