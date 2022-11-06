4ebe594b88ee3677d0e080e4f7b6388aa0eb801c
Matthias Kopeinigg <matkollin@gmail.com>
2022-11-06 06:33:55 +0100

    Add unit test for payments

--- src/test/java/at/matkollin/service/tebex/controller/WebhookControllerTest.java
+++ src/test/java/at/matkollin/service/tebex/controller/WebhookControllerTest.java
@@ -1,5 +1,6 @@
 package at.matkollin.service.tebex.controller;
 
+import at.matkollin.service.tebex.dto.webhook.PaymentDTO;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,7 +18,35 @@ public class WebhookControllerTest {
   @Test
   @DisplayName("Test webhook")
   public void testWebhook() throws Exception {
+    PaymentDTO paymentDTO = PaymentDTO.builder()
+            .transactionId("123")
+            .status(PaymentDTO.Status.builder()
+                    .id(1)
+                    .description("unknown")
+                    .build())
+            .paymentSequence("1")
+            .createdAt("2020-01-01")
+            .price(PaymentDTO.Price.builder()
+                    .amount(1.0)
+                    .currency("EUR")
+                    .build())
+            .customer(PaymentDTO.Customer.builder()
+                    .ip("127.0.0.1")
+                    .country("AT")
+                    .email("test@gmail.com")
+                    .firstName("Max")
+                    .lastName("Mustermann")
+                    .marketingConsent(true)
+                    .postalCode("1234")
+                    .username(PaymentDTO.Customer.Username.builder()
+                            .username("test")
+                            .id(1)
+                            .build())
+                    .postalCode("1234")
+                    .build())
+            .build();
 
+    this.webhookController.webhook(paymentDTO);
   }
 
 }
