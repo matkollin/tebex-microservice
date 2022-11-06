6bca02801e2dd5a92c06537c4e9ecd31826016f4
Matthias Kopeinigg <matkollin@gmail.com>
2022-11-06 21:42:28 +0100

    Add method to test the encoding

--- src/test/java/at/matkollin/service/tebex/service/ValidationServiceTest.java
+++ src/test/java/at/matkollin/service/tebex/service/ValidationServiceTest.java
@@ -3,6 +3,7 @@ package at.matkollin.service.tebex.service;
 import at.matkollin.service.tebex.controller.WebhookController;
 import at.matkollin.service.tebex.dto.validation.ValidationDTO;
 import at.matkollin.service.tebex.dto.validation.ValidationResponseDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,6 +14,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Optional;
 
+@Slf4j
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = WebhookController.class)
 public class ValidationServiceTest {
@@ -23,6 +25,9 @@ public class ValidationServiceTest {
   @Test
   @DisplayName("Test wrong authentication address")
   public void testAuthenticatedAddresses() {
+    // Set ip addresses since environment variables are not set in tests
+    this.validationService.setAuthorizedIpAddresses(new String[]{"18.209.80.3"});
+
     String address = "127.0.0.1";
     Assertions.assertFalse(this.validationService.isIpAddressAuthorized(address));
 
@@ -53,6 +58,22 @@ public class ValidationServiceTest {
     Assertions.assertNotNull(validationResponseDTO);
     Assertions.assertNotNull(validationResponseDTO.getId());
     Assertions.assertEquals(validationResponseDTO.getId(), validationDTO.getId());
+  }
+
+  @Test
+  @DisplayName("Test the signature of a webhook")
+  public void testSignature() {
+    this.validationService.setTebexWebhookSecret("secret");
+
+    String body = "test";
+    String signature = this.validationService.encodeMessage("secret-wrong", body);
+    Assertions.assertNotNull(signature);
+    Assertions.assertFalse(this.validationService.isSignatureValid(signature, body));
+
+    signature = this.validationService.encodeMessage("secret", body);
+    Assertions.assertNotNull(signature);
+    Assertions.assertTrue(this.validationService.isSignatureValid(signature, body));
+
   }
 
 }
