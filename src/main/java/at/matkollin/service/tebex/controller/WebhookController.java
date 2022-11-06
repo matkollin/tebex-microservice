93d270752fe1ec4fce923421fd8a72355c5cc5ee
Matthias Kopeinigg <matkollin@gmail.com>
2022-11-06 01:02:36 +0100

    Add payment dto to the webhook endpoint

--- src/main/java/at/matkollin/service/tebex/controller/WebhookController.java
+++ src/main/java/at/matkollin/service/tebex/controller/WebhookController.java
@@ -1,17 +1,22 @@
 package at.matkollin.service.tebex.controller;
 
+import at.matkollin.service.tebex.dto.webhook.PaymentDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RequestMapping("/tebex")
 @RestController
 public class WebhookController {
 
   @PostMapping("/webhook")
-  public ResponseEntity<HttpStatus> webhook() {
+  public ResponseEntity<HttpStatus> webhook(@RequestBody PaymentDTO paymentDTO) {
+    log.info("Received webhook: {}", paymentDTO);
     return ResponseEntity.ok().build();
   }
 
